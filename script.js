103562fcc0f8d828d783aa19e1d782489962bc24
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-09-08 19:33:11 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('arrow1');
+const leftBtn = document.getElementById('arrow2');
+
+let pictures = ['img/hero1.png', 'img/hero2.png', 'img/hero3.png'];
+
+img.src = pictures[0];
+let position = 0;
+
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
+
+
+function openOffer(evt, offerName) {
+
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(offerName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
